76747432fac55b76ad78b32826a9dca431f1293c
Abdallah Eid <abdallaheeid@icloud.com>
2025-11-23 01:32:42 +0100

    init

---
+++ src/test/java/org/abdallah/bloggingplatformapi/BloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abdallah.bloggingplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
